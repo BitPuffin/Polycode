b137ff4d9fa892bcbe2d4aa71a6a2f3013a65161
Isak Andersson <BitPuffin@lavabit.com>
2013-05-24 06:33:26 +0200

    Start writing PolycodeEntityEditor header file with new approach (dont copy and paste)

--- IDE/Contents/Include/PolycodeEntityEditor.h
+++ IDE/Contents/Include/PolycodeEntityEditor.h
@@ -19,3 +19,122 @@
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  THE SOFTWARE.
  */
+
+#pragma once
+
+#include "PolycodeEditor.h"
+#include <Polycode.h>
+#include <PolycodeUI.h>
+#include "PolycodeProps.h"
+
+using namespace Polycode;
+
+#ifdef _WINDOWS
+#define round (int)
+#endif
+
+class EntityBrowserData  {
+	public:
+		Entity *entity;
+};
+
+class PolycodeEntityEditorActionDataEntry {
+	public:
+		PolycodeEntityEditorActionDataEntry(){
+			entity = NULL;
+			parentEntity = NULL;
+		}
+		PolycodeEntityEditorActionDataEntry(Vector3 vec3, Number number);	
+		PolycodeEntityEditorActionDataEntry(Vector3 vec3);
+		PolycodeEntityEditorActionDataEntry(Number number);
+		PolycodeEntityEditorActionDataEntry(Entity *entity);
+		Vector3 vec3;
+		Number number;
+		Entity *entity;
+		Entity *parentEntity;		
+};
+
+class PolycodeEntityEditorActionData : public PolycodeEditorActionData {
+	public:
+		PolycodeEntityEditorActionData() {
+			reverse = true;
+		}
+		virtual ~PolycodeEntityEditorActionData() {}
+
+		std::vector<PolycodeEntityEditorActionDataEntry> enties;
+		PolycodeEntityEditorActionDataEntry entry;
+		bool reverse;
+}
+
+class EntityTreeView : public UIElement {
+	public:
+		EntityTreeView(Entity *rootEntry);
+		~EntityTreeView();
+
+		void Resize(Number width, Number height);
+		void handleEvent(Event *event);
+
+		void syncNodeToEntity(UITree *node, Entity *entity);
+
+		void Refresh();
+
+		Entity *selectedEntity;
+		Entity *targetLayer;
+
+		UIImageButton *newLayerButton;
+		UIImageButton *targetLayerButton;
+
+
+	protected:
+
+		Entity *rootEntity;
+
+		bool dontSendSelectionEvent;
+
+		UITreeContainer *treeContainer;
+
+		ScreenShape *headerBg;
+		ScreenShape *bg;
+}
+
+class PropertiesSizer : public UIElement {
+	public:
+		PropertiesSizer();
+		~PropertiesSizer();
+
+		void Resize(Number width, Number height);
+
+		UIVSizer *sizer;
+}
+
+
+class EntityNameDisplay : public ScreenEntity {
+	public:
+		EntityNameDisplay(Entity *targetEntity);
+		~EntityNameDisplay();
+
+		void Update();
+
+		ScreenLabel *label;
+		ScreenLabel *tagsLabel;
+		Entity *targetEntity;
+}
+
+class PolycodeEntityEditorMain : public UIElement {
+	public:
+		PolycodeEntityEditorMain(PolycodeEditor *editor);
+		virtual ~PolycodeScreenEditorMain();
+
+		void Resize(Number width, Number height);
+		virtual void syncTransformToSelected();
+		Entity *addNewLayer(String layerName);
+		void updateCursor();
+		virtual void selectEntity(Entity *entity, bool doAction = true); 	// Perhaps make this one non-virtual and add an onSelect callback
+		virtual void resetSelectedEntityTransforms() = 0;
+		void setMode(int newMode);
+		virtual void handleEvent(Event *event);
+		void resizePreviewScreen();
+		void handleDroppedFile(OSFileEntry file, Number x, Number y);
+		bool hasSelected(Entity *entity);
+
+}
